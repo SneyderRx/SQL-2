f3c0f7d63e7464bb848469fa35cfb6e0690ddc3d
SneyderRx <128755804+SneyderRx@users.noreply.github.com>
2025-06-17 09:29:51 -0500

    feat: :sparkles: ultimo

--- querys.sql
+++ querys.sql
@@ -67,3 +67,24 @@ END $$
 DELIMITER ;
 
 SELECT fn_pedido_mayor();
+
+DELIMITER $$
+DROP FUNCTION IF EXISTS fn_pedidos_year $$
+
+CREATE FUNCTION fn_pedidos_year(year_1 DATE, year_2 DATE)
+RETURNS INT
+NOT DETERMINISTIC
+READS SQL DATA
+BEGIN
+    DECLARE v_pedidos INT;
+
+    SELECT COUNT(id) INTO v_pedidos
+    FROM pedido
+    WHERE fecha BETWEEN year_1 AND year_2;
+
+    RETURN v_pedidos;
+END $$
+
+DELIMITER ;
+
+SELECT fn_pedidos_year('2023-01-01', '2024-01-01');
